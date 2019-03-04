02e0bede440e4c3dda46253050e70513b08d0c94
manpreet rai <manpreetrai1912@hotmail.com>
2019-03-04 15:51:58 -0500

    add for loop

--- C0742302/js/main.js
+++ C0742302/js/main.js
@@ -1,1 +1,18 @@
 ﻿// Your code here!
+let addnumbers = {
+   
+    StartNumber=1109,
+    StepDownNo=11,
+    endNumber=119,
+    adder: function () {
+        var sum = 0;
+
+        for (var i = this.StartNumber; i >= this.endNumber;) {
+            sum += i;
+            i -= this.StepDownNo;
+        }
+        document.write("sum is:" + sum);
+
+    }
+
+}
